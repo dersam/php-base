51ba7e9b7553c264d931babe3294672dc37c4eeb
Sam Schmidt <samuel@dersam.net>
2018-04-24 19:35:21 -0400

    Add RoboFile

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Simple base repo for toy projects
 Objective: something that works without any real setup beyond PHP being installed and `composer install`
 
 - Codeception for testing: `composer test`
-- Robo for CLI tasks
+- Robo for CLI tasks: `./vendor/bin/robo`
 - PHP built-in webserver for trying things that need a webserver: `composer serve`
---
+++ RoboFile.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    /**
+     * The bot says hello
+     */
+    public function exampleGreeting()
+    {
+        $this->say((new \Dersam\ExampleClass())->getGreeting());
+    }
+
+    /**
+     * The bot says whatever
+     * @param $message string The message to say
+     */
+    public function exampleEcho($message)
+    {
+        $this->say($message);
+    }
+}
